181d052cbe2bc1d57d69e893babbf37ea3169a51
TanLocIT200600 <77155886+TanLocIT200600@users.noreply.github.com>
2021-05-07 11:15:48 +0700

    z

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult ListEmployee()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        QLNS_1Entities database = new QLNS_1Entities();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult CheckLogin(TaiKhoan taiKhoan)
+        {
+            var check = database.TaiKhoans.Where(s => s.TenTK == taiKhoan.TenTK && s.MatKhau == taiKhoan.MatKhau).SingleOrDefault();
+            if (check == null)
+            {
+                ViewBag.ErrorInfo = "Sai thông tin";
+                return View("Login");
+            }
+            else
+            {
+                database.Configuration.ValidateOnSaveEnabled = false;
+                Session["TenTK"] = taiKhoan.TenTK;
+                Session["MatKhau"] = taiKhoan.MatKhau;
+                return RedirectToAction("ListEmployee", "Home");
+            }
+            return View();
+        }
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}

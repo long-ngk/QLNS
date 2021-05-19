2b1be6c238eb990ae23e0411442f18680cf40778
minhlan26 <74177482+minhlan26@users.noreply.github.com>
2021-05-19 10:21:31 +0700

    Actor phòng nhân sự
    Hoàn thành:
    - Quản lý phòng ban gồm giao diện + chức năng (thêm, xóa, sửa, tìm kiếm, xem danh sách)
    - Quản lý chức vụ gồm giao diện + chức năng (thêm, xóa, sửa, tìm kiếm, xem danh sách)

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# QLNS
+# WEBSITE QUẢN LÝ NHÂN SỰ
+
+## Danh sách thành viên
+
+- Thành viên 1: Nguyễn Kim Long 18DH110293
+- Thành viên 2: Nguyễn Tấn Lộc 18DH110280
+- Thành viên 3: Nguyễn Minh Lân 18DH110950
+
+## Mô tả tóm tắt đề tài
+
+- Đây là website giúp người trưởng phòng nhân sự có thể quản lý nhân viên của mình.
+- Người dùng cần có tài khoản, mật khẩu riêng để có thể đăng nhập/đăng xuất website.
+- Gồm các chức năng chính dành riêng cho trưởng phòng nhân sự:
+	- Quản lý hồ sơ nhân viên (thêm, xóa, sửa, xem danh sách)
+	- Quản lý chấm công (tính ngày công theo tháng, theo tăng ca)
+	- Danh sách khen thưởng/kỉ luật nhân viên
+	- Quản lý tiền lương nhân viên (tính lương tăng ca, tổng lương nhận)
+- Ngoài ra website còn dành cho đối tượng sử dụng là nhân viên dùng để xem thông tin cá nhân, đổi mật khẩu tài khoản, chấm công ngày làm.
+
---
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Extensions;
+using System.Net.Mail;
+using System.Net;
+using System.Web.Helpers;
+using WebApplication1.Models.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        QLNhanSuEntities database = new QLNhanSuEntities();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult CheckLogin(TaiKhoanViewModel taiKhoan)
+        {
+            var check = database.TaiKhoans.Where(s => s.TenTK == taiKhoan.TenTK && s.MatKhau == taiKhoan.MatKhau).SingleOrDefault();
+            var trangThaiLamViec = database.TaiKhoans.Where(s => s.TenTK == taiKhoan.TenTK && s.MatKhau == taiKhoan.MatKhau && s.NhanVien.TrangThai == true).SingleOrDefault();
+            if (taiKhoan.TenTK == null || taiKhoan.MatKhau == null || taiKhoan.TenTK == "" || taiKhoan.MatKhau == "")
+            {
+                return View("Login");
+            }
+            else
+            {
+                if (check == null)
+                {
+                    this.AddNotification("Sai tên tài khoản hoặc mật khẩu!", NotificationType.ERROR);
+
+                    return View("Login");
+                }
+                else
+                {
+                    if (trangThaiLamViec != null)
+                    {
+                        database.Configuration.ValidateOnSaveEnabled = false;
+                        var tenNhanVien = (from a in database.TaiKhoans
+                                           where a.TenTK == taiKhoan.TenTK
+                                           select a.NhanVien.HoTen).SingleOrDefault();
+                        if (tenNhanVien != null)
+                        {
+                            Session["TenNhanVien"] = tenNhanVien.ToString();
+                        }
+                        
+                        Session["TenTK"] = taiKhoan.TenTK;
+                        Session["MatKhau"] = taiKhoan.MatKhau;
+                        switch (check.MaQuyen)
+                        {
+                            case 4:
+                                return RedirectToAction("ListEmployee", "Home");
+                            case 3:
+                                return RedirectToAction("Index", "TaiKhoan");
+                        }
+                        return RedirectToAction("ListEmployee", "Home");
+                    }
+                    else
+                    {
+                        this.AddNotification("Tài khoản đã bị vô hiệu hóa.", NotificationType.ERROR);
+                        return View("Login");
+                    }
+                }
+            }
+        }
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public ActionResult CheckForgotPassword(CheckTenTK_Email check, string command)
+        {
+            var checkTenTK = database.TaiKhoans.Where(s => s.TenTK == check.TenTK).SingleOrDefault();
+            var checkEmail = database.NhanViens.Where(s => s.Email == check.Email).SingleOrDefault();
+            if (command.Equals("Gửi mail"))
+            {
+                if (check.TenTK == null || check.Email == null || check.TenTK == "" || check.Email == "")
+                {
+                    return View("ForgotPassword");
+                }
+                else
+                {
+
+                    if (checkTenTK == null || checkEmail == null)
+                    {
+                        this.AddNotification("Sai tên tài khoản hoặc email!", NotificationType.ERROR);
+                        return View("ForgotPassword");
+                    }
+                    else
+                    {
+                        string resetCode = Guid.NewGuid().ToString();
+                        SendResetPasswordLinkEmail(checkEmail.Email, resetCode);
+                        checkTenTK.ResetPasswordCode = resetCode;
+                        this.AddNotification("Đã gửi mail. Vui lòng kiểm tra email!", NotificationType.SUCCESS);
+                        
+                        database.Configuration.ValidateOnSaveEnabled = false;
+                        database.SaveChanges();
+                        return View("ForgotPassword");
+                    }
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+        }
+
+        [NonAction]
+        public void SendResetPasswordLinkEmail(string email, string resetCode)
+        {
+            var verifyUrl = "/Login/ResetPassword/" + resetCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("democnpmnc@gmail.com", "Long Nguyen");
+            var toEmail = new MailAddress(email);
+            var fromEmailPassword = "democnpmnc123";
+            string subject = "Reset password";
+            string body = "Chào. Nhấp vào link kế bên để cài lại mật khẩu. <a href=" + link + ">Link đây</a>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            }) smtp.Send(message);
+        }
+
+        public ActionResult ResetPassword(string id)
+        {
+            QLNhanSuEntities database = new QLNhanSuEntities();
+            var taiKhoan = database.TaiKhoans.Where(s => s.ResetPasswordCode == id).FirstOrDefault();
+            if(taiKhoan != null)
+            {
+                ResetPasswordModel model = new ResetPasswordModel();
+                model.ResetCode = id;
+                return View(model);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ResetPassword(ResetPasswordModel model)
+        {
+            var message = "";
+
+            if (ModelState.IsValid)
+            {
+                QLNhanSuEntities database = new QLNhanSuEntities();
+                var taiKhoan = database.TaiKhoans.Where(s => s.ResetPasswordCode == model.ResetCode).FirstOrDefault();
+                if(taiKhoan != null)
+                {
+                    taiKhoan.MatKhau = model.MatKhauMoi;
+                    taiKhoan.ResetPasswordCode = "";
+                    database.Configuration.ValidateOnSaveEnabled = false;
+                    database.SaveChanges();
+                    this.AddNotification("Mật khẩu được thay đổi thành công.", NotificationType.SUCCESS);
+                }
+                else
+                {
+                    this.AddNotification("Phiên đặt lại mật khẩu đã hết hạn. Vui lòng thử lại!", NotificationType.ERROR);
+                }
+            }
+            else
+            {
+                this.AddNotification("Có gì đó sai sai...", NotificationType.ERROR);
+            }
+            ViewBag.Message = message;
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
